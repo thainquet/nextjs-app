a4f2627c7436fc802afeef9e2f3a024773ebdf73
thainquet <thainq00@gmail.com>
2020-12-07 00:19:32 +0700

    fix read file with fs

--- pages/[fileName].js
+++ pages/[fileName].js
@@ -1,18 +1,36 @@
 import react from 'react'
+import matter from "gray-matter";
+import ReactMarkdown from "react-markdown";
+import { FolderName } from '../configFolder'
 
-const Post = ({props}) => {
+const Post = props => {
+  console.log(props)
   return (<>
-    <div>Post: {props.file}</div>
+    <div>Post:</div>
+    <ReactMarkdown
+      escapeHtml={false}
+      source={props.content}
+    />
   </>)
 }
 
 Post.getInitialProps = async (context) => {
-  let {fileName} = context.query
-
-  return {
-    ...{
-      file: fileName
+  let { fileName } = context.query
+  const fs = require('fs')
+  const path = require('path')
+  let content = ""
+  FolderName.forEach(fn => {
+    const link = path.join(process.cwd(), fn, fileName + ".md")
+    if (fs.existsSync(link)) {
+      content = fs.readFileSync(link, {
+        encoding: "utf-8",
+      });
+      console.log(content)
     }
+  })
+  return {
+    file: fileName,
+    content
   }
 }
 
